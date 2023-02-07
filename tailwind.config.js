d33df2777e6e29343d4bb786909b74005802beee
wakataw <pratamaagung@hotmail.com>
2023-02-07 22:46:41 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '*.html',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'djp-blue': '#212C5F',
+        'djp-blue-dark': '#070F32',
+        'djp-blue-light': '#1D428A',
+        'djp-yellow': '#FFC91B',
+        'djp-yellow-dark': '#C89221',
+        'djp-yellow-light': '#FFE804'
+      },
+    },
+  },
+  plugins: [],
+}
